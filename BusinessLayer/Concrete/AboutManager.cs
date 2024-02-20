6b12bae09f355a0726daa43d3c61169655c97f85
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-02-20 21:43:31 +0300

    Abstract and concrete folders required for N-Tier architecture have been created.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager:IAboutService
+    {
+        IAboutDal _aboutdal;
+
+        public AboutManager(IAboutDal aboutdal)
+        {
+            _aboutdal = aboutdal;
+        }
+
+        public void TAdd(About entity)
+        {
+            _aboutdal.Insert(entity);
+        }
+
+        public void TDelete(About entity)
+        {
+            _aboutdal.Delete(entity);
+        }
+
+        public void TUpdate(About entity)
+        {
+            _aboutdal.Update(entity);
+        }
+
+
+        public About TGetByID(int id)
+        {
+            return _aboutdal.GetByID(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutdal.GetList();
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+//}
