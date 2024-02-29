846fa8911981ac3c49b67a4460b785c2c704e977
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-02-29 18:53:48 +0300

    Portfolio CRUD operations are complete.

---
+++ Core_Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace Core_Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);
+            portfolioManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            portfolioManager.TUpdate(portfolio);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Core_Portfolio/Program.cs
+++ Core_Portfolio/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Experience}/{action=Index}/{id?}");
+    pattern: "{controller=Portfolio}/{action=Index}/{id?}");
 
 app.Run();
