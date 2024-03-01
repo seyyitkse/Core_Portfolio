752a1adabbdd545344d6b3a7dc58e95d9d72b483
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-01 14:27:44 +0300

    CRUD and validation operations were created for the service class.

---
+++ BusinessLayer/ValidationRules/ServiceValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ServiceValidator:AbstractValidator<Service>
+    {
+        public ServiceValidator()
+        {
+            RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("The title field cannot be left blank")
+            .MinimumLength(5).WithMessage("The title must consist of at least 5 characters.")
+            .MaximumLength(50).WithMessage("The title must have a maximum of 50 characters.");
+
+            RuleFor(x => x.ImageURL)
+                .NotEmpty().WithMessage("The URL field cannot be left blank")
+                .MinimumLength(15).WithMessage("The title must consist of at least 5 characters.")
+                .MaximumLength(500).WithMessage("The title must have a maximum of 500 characters.");
+        }
+    }
+}
---
+++ Core_Portfolio/Controllers/ServiceController.cs
@@ -0,0 +1,76 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddService(Service service) 
+        {
+            ServiceValidator validations=new ServiceValidator();
+            ValidationResult results = validations.Validate(service);
+            if (results.IsValid)
+            {
+                serviceManager.TAdd(service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteService(int id)
+        {
+            var values=serviceManager.TGetByID(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateService(int id)
+        {
+            var values = serviceManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateService(Service service)
+        {
+            ServiceValidator validations= new ServiceValidator();
+            ValidationResult results = validations.Validate(service);
+            if (results.IsValid)
+            {
+                serviceManager.TUpdate(service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
