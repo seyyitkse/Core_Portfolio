b420d23e49e9eb58381f1bef9022b21cd60c738c
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-02 17:02:32 +0300

    The necessary files for the theme change have been added. (Since there are some desired features on the existing theme, the transition to the CORONA Admin Theme has been made).

---
+++ Core_Portfolio/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
