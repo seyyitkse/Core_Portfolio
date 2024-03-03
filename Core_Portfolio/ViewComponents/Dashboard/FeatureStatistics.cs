af8dbcb8f0e89b314cbe0bf81b5280dfb3154d66
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-03 21:04:33 +0300

    Dashboard editing has started.

---
+++ Core_Portfolio/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+    public class FeatureStatistics:ViewComponent
+    {
+        Context context=new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SkillQuantity=context.Skills.Count();
+            ViewBag.Unread=context.Messages.Where(x=>x.Status==true).Count();
+            ViewBag.Read=context.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.Experience = context.Experiences.Count();
+            return View();
+        }
+    }
+}
---
+++ Core_Portfolio/ViewComponents/Dashboard/LastProjects.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+    public class LastProjects:ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var portfolios=portfolioManager.TGetList().OrderByDescending(p => p.PortfolioID).Take(5).ToList();
+            return View(portfolios);
+        }
+    }
+}
