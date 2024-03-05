0dd094678876cb0f5c46d59b26de0e51a9e53cff
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-05 08:39:12 +0300

    Map section moved to view component.

---
+++ Core_Portfolio/ViewComponents/Dashboard/VisitorPanel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+    public class VisitorPanel:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
