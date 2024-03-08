45a1832ad6ea6b878d9a51fac5d1c8f24f32f05d
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-08 10:54:47 +0300

    Announcement and announcement detail page completed (Announcements are created by admin and visible to authors).

---
+++ BusinessLayer/Concrete/AnnouncementManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+        IAnnouncementDal _announcementdal;
+
+        public AnnouncementManager(IAnnouncementDal announcementdal)
+        {
+            _announcementdal = announcementdal;
+        }
+
+        public void TAdd(Announcement entity)
+        {
+            _announcementdal.Insert(entity);
+        }
+
+        public void TDelete(Announcement entity)
+        {
+            _announcementdal.Delete(entity);
+        }
+
+        public Announcement TGetByID(int id)
+        {
+            return _announcementdal.GetByID(id);
+        }
+
+        public List<Announcement> TGetList()
+        {
+            return _announcementdal.GetList();
+        }
+
+        public void TUpdate(Announcement entity)
+        {
+            _announcementdal.Update(entity);
+        }
+    }
+}
--- Core_Portfolio/Areas/Writer/Controllers/DefaultController.cs
+++ Core_Portfolio/Areas/Writer/Controllers/DefaultController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Portfolio.Areas.Writer.Controllers
@@ -7,9 +10,17 @@ namespace Core_Portfolio.Areas.Writer.Controllers
     [Authorize]
     public class DefaultController : Controller
     {
+        AnnouncementManager announcement = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
-            return View();
+            var values=announcement.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AnnouncementDetails(int id)
+        {
+            Announcement values=announcement.TGetByID(id);
+            return View(values);
         }
     }
 }
