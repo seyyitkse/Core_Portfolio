ae31104d46ebb1ece49cc9029487dedbf2923ea3
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-13 14:29:54 +0300

    Contact information CRUD operations completed.

---
+++ Core_Portfolio/Controllers/SubContactController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.Controllers
+{
+    public class SubContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        public IActionResult Index()
+        {
+            var values = contactManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult UpdateSubContact(int id)
+        {
+            var values = contactManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateSubContact(Contact contact)
+        {
+                contactManager.TUpdate(contact);
+                return RedirectToAction("Index");
+        }
+    }
+}
