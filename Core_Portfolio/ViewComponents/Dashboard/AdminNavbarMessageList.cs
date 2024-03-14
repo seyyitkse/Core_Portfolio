5bfd572866558acc322f152f12e1a82aa3052b9b
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-14 13:33:29 +0300

    Various edits have been made in the admin panel.

---
+++ Core_Portfolio/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList:ViewComponent
+    {
+        WriterMessageManager messageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            string mail = "admin@mail.com";
+            var values = messageManager.GetListRecevierMessages(mail).OrderByDescending(x => x.WriterMessageID).Where(x=>x.Recevier==mail).Take(3).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ Core_Portfolio/ViewComponents/Dashboard/AdminNavbarNotificationList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Portfolio.ViewComponents.Dashboard
+{
+    public class AdminNavbarNotificationList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
