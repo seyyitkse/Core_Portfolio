23edd5289ae5ed8bea4e35cef88439193e4becff
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-15 13:56:52 +0300

    Dynamic listing in the console in AJAX.

---
+++ BusinessLayer/Concrete/WriterUserManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterUserManager : IWriterUserService
+    {
+        IWriterUserDal _writerUserDal;
+
+        public WriterUserManager(IWriterUserDal writerUserDal)
+        {
+            _writerUserDal = writerUserDal;
+        }
+
+        public void TAdd(WriterUser entity)
+        {
+            _writerUserDal.Insert(entity);
+        }
+
+        public void TDelete(WriterUser entity)
+        {
+            _writerUserDal.Delete(entity);
+        }
+
+        public WriterUser TGetByID(int id)
+        {
+            return _writerUserDal.GetByID(id);
+        }
+
+        public List<WriterUser> TGetList()
+        {
+            return _writerUserDal.GetList();
+        }
+
+        public void TUpdate(WriterUser entity)
+        {
+            _writerUserDal.Update(entity);
+        }
+    }
+}
--- Core_Portfolio/Controllers/WriterUserController.cs
+++ Core_Portfolio/Controllers/WriterUserController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Core_Portfolio.Controllers
 {
     public class WriterUserController : Controller
     {
+        WriterUserManager writerUserManager = new WriterUserManager(new EfWriterUserDal());
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult ListUser()
+        {
+            var values = JsonConvert.SerializeObject(writerUserManager.TGetList());
+            return Json(values);
+        } 
     }
 }
