e1ec04ea1cbf4525ef956eceaf049f8465751a6c
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-16 21:39:55 +0300

    CRUD operations related to Experience were done with AJAX.(Update process will be looked at separately.)

---
+++ Core_Portfolio/Controllers/Experience2Controller.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_Portfolio.Controllers
+{
+    [AllowAnonymous]
+    public class Experience2Controller : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            var values=JsonConvert.SerializeObject(experience);
+            return Json(values);
+        }
+        public IActionResult GetById(int ExperienceID) 
+        {
+            var v = experienceManager.TGetByID(ExperienceID);
+            var values=JsonConvert.SerializeObject(v);
+            return Json(values);
+        }
+        public IActionResult DeleteExperience(int id) 
+        {
+            var v = experienceManager.TGetByID(id);
+            experienceManager.TDelete(v);
+            return NoContent();
+        }
+    }
+}
