4cfa30e24021e289f2265bab585e4d463d8d5087
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-17 09:18:35 +0300

    404 Error page created.

--- Core_Portfolio/Controllers/ErrorPageController.cs
+++ Core_Portfolio/Controllers/ErrorPageController.cs
@@ -8,5 +8,9 @@ namespace Core_Portfolio.Controllers
         {
             return View();
         }
+        public IActionResult Error404() 
+        {
+            return View();
+        }
     }
 }
--- Core_Portfolio/Program.cs
+++ Core_Portfolio/Program.cs
@@ -41,6 +41,7 @@ if (!app.Environment.IsDevelopment())
 }
 
     
+    app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404");
 
     app.UseHttpsRedirection();
     app.UseStaticFiles();
